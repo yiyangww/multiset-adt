8e594a2f706867e392ba5c56b159e56c72945069
yiyangww <yiyangww@gmail.com>
2024-09-16 07:53:59 -0400

    update LinkedListMutiSet, add Node translation

--- src/LinkedListMultiSet.java
+++ src/LinkedListMultiSet.java
@@ -1,45 +1,84 @@
 import java.util.LinkedList;
 
 public class LinkedListMultiSet<T> implements MultiSet<T> {
-    private LinkedList<T> list;
+    private Node<T> front;
+    private int size;
 
     public LinkedListMultiSet() {
-        list = new LinkedList<>();
+        this.front = null;
+        this.size = 0;
     }
 
     @Override
     public boolean add(T item) {
-        return list.add(item);
+        Node<T> newNode = new Node<>(item);
+        newNode.next = front;
+        front = newNode;
+        size++;
+        return true;
     }
 
     @Override
     public void remove(T item) {
-        list.remove(item);
+        Node<T> cur = front;
+        Node<T> prev = null;
+        while (cur != null) {
+            if (cur.item.equals(item)) {
+                size--;
+                if (prev != null) {
+                    prev.next = cur.next;
+                } else { // first item
+                    front = cur.next;
+                }
+                return;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
     }
 
     @Override
     public boolean contains(T item) {
-        return list.contains(item);
+        Node<T> cur = front;
+        while (cur != null) {
+            if (cur.item.equals(item)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return list.isEmpty();
+        return front == null;
     }
 
     @Override
     public int count(T item) {
-        int count = 0;
-        for (T t : list) {
-            if (t.equals(item)) {
-                count++;
+        int numSeen = 0;
+        Node<T> cur = front;
+        while (cur != null) {
+            if (cur.item.equals(item)) {
+                numSeen++;
             }
+            cur = cur.next;
         }
-        return count;
+        return numSeen;
     }
 
     @Override
     public int size() {
-        return list.size();
+        return size;
+    }
+
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+
+        Node(T item) {
+            this.item = item;
+            this.next = null;
+        }
     }
 }
